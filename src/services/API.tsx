8bab0b32c92954f3d6171338632cf4b993e5c3e0
JoMndz22 <josue.mendez087@gmail.com>
2022-06-28 21:28:29 -0600

    create api TSX

---
+++ src/services/API.tsx
@@ -0,0 +1,22 @@
+import {API_KEY, BASE_MOVIE_URL} from '../config';
+
+interface GetData {
+  url: string;
+  search: boolean;
+}
+
+export const GET = async ({url, search = false}: GetData) => {
+  let URL_API;
+
+  !search
+    ? (URL_API = `${BASE_MOVIE_URL}${url}?api_key=${API_KEY}`)
+    : (URL_API = `${BASE_MOVIE_URL}${url}`);
+
+  let response: any = await fetch(URL_API, {
+    method: 'GET',
+  });
+
+  response = response.json();
+
+  return response;
+};
