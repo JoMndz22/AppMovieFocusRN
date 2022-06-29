4dc5ebbca915cca13169898d20f55c48fce68431
JoMndz22 <josue.mendez087@gmail.com>
2022-06-28 21:27:40 -0600

    fix app TS & configs Urls

---
+++ src/components/Navigation.js
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/Home/HomeScreen.js';
+import LoginScreen from '../screens/Login/LoginScreen';
+
+import { AuthContext } from '../context/AuthContext';
+import MovieDetails from '../screens/Movie/MovieDetails';
+import SearchScreen from '../screens/Search/SearchScreen';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = (props) => {
+
+    const { userInfo } = useContext(AuthContext);
+
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {
+                    userInfo.token
+                        ?
+                        <>
+                            <Stack.Screen
+                                name="Home"
+                                component={HomeScreen}
+                                options={{ headerShown: false }}
+                            />
+                            <Stack.Screen
+                                name="MovieDetails"
+                                component={MovieDetails}
+                                options={{ headerShown: false }}
+                            />
+                            <Stack.Screen
+                                name="SearchScreen"
+                                component={SearchScreen}
+                                options={{ headerShown: false }}
+                            />
+
+                        </>
+                        :
+                        <Stack.Screen
+                            name="Login"
+                            component={LoginScreen}
+                            options={{ headerShown: false }}
+                        />
+
+                }
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+
+};
+
+export default Navigation;
